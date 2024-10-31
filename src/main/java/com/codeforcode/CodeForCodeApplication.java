cc688bd1d943457178a82851f449d2532e853120
Duskafka <rlarlgus0206@naver.com>
2024-10-31 17:19:20 +0900

    init commit

---
+++ src/main/java/com/codeforcode/CodeForCodeApplication.java
@@ -0,0 +1,13 @@
+package com.codeforcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeForCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeForCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeforcode/CodeForCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeforcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeForCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
