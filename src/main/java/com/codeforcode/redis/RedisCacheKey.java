71c39c74e6b2ab10f71546edd2098f7d71503c00
Duskafka <rlarlgus0206@naver.com>
2024-11-02 13:28:43 +0900

    feat(Redis): RedisFactory 생성

---
+++ src/main/java/com/codeforcode/redis/RedisCacheKey.java
@@ -0,0 +1,6 @@
+package com.codeforcode.redis;
+
+public class RedisCacheKey {
+    public static final String STUDY_GROUP_LIST = "studyGroupList_userId";
+    public static final String RESERVATION_LIST = "reservationList_roomId";
+}
---
+++ src/main/java/com/codeforcode/redis/RefreshTokenRepository.java
@@ -0,0 +1,12 @@
+package com.codeforcode.redis;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+
+    Optional<RefreshToken> findByToken(String token);
+
+    Optional<RefreshToken> findByAuthId(String authId);
+}
